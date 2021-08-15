f029e0afca5f7e01e575c05d8bf60950bce9d5fa
髙橋和晃 <kazuaki.flute@gmail.com>
2021-08-15 12:01:51 +0900

    use MyComponent component in App component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <br>
+  <MyComponent msg="This is MyComponent!" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyComponent
   }
 })
 </script>
