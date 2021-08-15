46a186dfe6307aa2f2e280a2f35f86624c07b704
髙橋和晃 <kazuaki.flute@gmail.com>
2021-08-15 12:22:37 +0900

    rename component to GitHubProfile

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <br>
-  <MyComponent accountName="Kazuchanfl" />
+  <GitHubProfile accountName="Kazuchanfl" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import MyComponent from './components/MyComponent.vue'
+import GitHubProfile from './components/GitHubProfile.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
-    MyComponent
+    GitHubProfile
   }
 })
 </script>
--- src/components/GitHubProfile.vue
+++ src/components/GitHubProfile.vue
@@ -12,7 +12,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'MyComponent',
+  name: 'GitHubProfile',
   props: {
     accountName: {
       type: String,
