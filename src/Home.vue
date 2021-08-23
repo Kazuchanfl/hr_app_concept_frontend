7ac0dabbab5a18029fa127482402fa35c3deaee3
髙橋和晃 <kazuaki.flute@gmail.com>
2021-08-24 08:54:08 +0900

    change component name Home

--- src/Home.vue
+++ src/Home.vue
@@ -12,7 +12,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'GitHubProfile',
+  name: 'Home',
   props: {
     accountName: {
       type: String,
