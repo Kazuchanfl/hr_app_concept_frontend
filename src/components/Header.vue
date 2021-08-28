4ab64f6731e32d3acbaba564448bbd684ddd9119
髙橋和晃 <kazuaki.flute@gmail.com>
2021-08-28 11:04:36 +0900

    use Logo Component in Header Component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,15 @@
 <template>
-  <h1>This is created by {{ accountName }}.</h1>
-
-  <p>
-    <a :href="githubLink + accountName" target="_blank">
-      {{ accountName }}'s GitHub Profile
-    </a>
-  </p>
-
+  <Logo></Logo>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Logo from './Logo.vue'
 export default defineComponent({
-  name: 'Home',
-  props: {
-    accountName: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-      const githubLink = `https://github.com/`
-      return { githubLink }
-  }
+  components: { Logo },
+  name: 'Header',
 })
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
