af061a846201a9277e5cb3699ff8a2c338dc86de
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-20 12:59:41 +0900

    refactor: remove Home.vue to pages folder

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import Home from './Home.vue'
+import Home from './pages/Home.vue'
 
 export default defineComponent({
   name: 'App',
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,7 +12,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Header from './components/Header.vue'
+import Header from '../components/Header.vue'
 
 export default defineComponent({
   components: { Header },
