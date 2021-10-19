93161e94586f06f20d10ba8f4e4f02e5bc000354
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-20 08:27:40 +0900

    add hamburger-menu

--- src/components/GlobalMenu.vue
+++ src/components/GlobalMenu.vue
@@ -1,4 +1,8 @@
 <template>
+<div class="hamburger-menu">
+  <input type="checkbox" id="menu-btn-check">
+  <label for="menu-btn-check" class="menu-btn"><span></span></label>
+</div>
 <div class="menu-background">
   <ul>
     <li class="logo"><Logo></Logo></li>
@@ -56,7 +60,41 @@ a:hover {
   color: #dddddd;
 }
 
+.menu-btn {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    display: flex;
+    height: 60px;
+    width: 60px;
+    justify-content: center;
+    align-items: center;
+    z-index: 90;
+    background-color: #3584bb;
+}
+.menu-btn span,
+.menu-btn span:before,
+.menu-btn span:after {
+    content: '';
+    display: block;
+    height: 3px;
+    width: 25px;
+    border-radius: 3px;
+    background-color: #ffffff;
+    position: absolute;
+}
+.menu-btn span:before {
+    bottom: 8px;
+}
+.menu-btn span:after {
+    top: 8px;
+}
+
 @media screen and (min-width: 600px) {
+  div.hamburger-menu {
+    display: none;
+  }
+
   div.menu-background {
     background: linear-gradient(#f19aff, #66f6ff);
     position: absolute;
