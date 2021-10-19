6f8ae35723320a47a4b77a33be4ffabefd9b9b2b
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-20 08:31:19 +0900

    make HamburgerMenu a component

---
+++ src/components/HamburgerMenu.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hamburger-menu">
+        <input type="checkbox" id="menu-btn-check">
+        <label for="menu-btn-check" class="menu-btn"><span></span></label>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HamburgerMenu',
+})
+</script>
+<style scoped>
+.menu-btn {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    display: flex;
+    height: 60px;
+    width: 60px;
+    justify-content: center;
+    align-items: center;
+    z-index: 90;
+    background-color: #3584bb;
+}
+.menu-btn span,
+.menu-btn span:before,
+.menu-btn span:after {
+    content: '';
+    display: block;
+    height: 3px;
+    width: 25px;
+    border-radius: 3px;
+    background-color: #ffffff;
+    position: absolute;
+}
+.menu-btn span:before {
+    bottom: 8px;
+}
+.menu-btn span:after {
+    top: 8px;
+}
+
+@media screen and (min-width: 600px) {
+  div.hamburger-menu {
+    display: none;
+  }
+}
+</style>
